afa39a41ee86b81fb07c8d2cddc5f2d24ac64d72
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2018-09-12 21:54:05 +0300

    Created base structure

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const  items = [
+    {
+        "id": 1,
+        "title": "Velit anim deserunt elit velit"
+    },
+    {
+        "id": 2,
+        "title": "Fugiat amet veniam cillum ut aliquip velit est esse minim fugiat eiusmod sint."
+    },
+    {
+        "id": 3,
+        "title": "Nisi sit nisi cillum dolor fugiat sint do nostrud ex cillum cupidatat. "
+    },
+    {
+        "id": 4,
+        "title": "Non amet amet ut magna culpa dolore consequat occaecat."
+    },
+    {
+        "id": 5,
+        "title": "Elit labore ad nisi anim adipisicing tempor velit commodo adipisicing ipsum ut."
+    }
+];
